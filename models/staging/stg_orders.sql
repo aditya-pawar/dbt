6348313fc3369d122ce3984951969959aeff2ffa
Aditya Pawar <codes.aditya@gmail.com>
2023-06-17 21:20:30 +0000

    devepoped stage models

---
+++ models/staging/stg_orders.sql
@@ -0,0 +1,22 @@
+select
+    -- from raw order
+    o.orderid,
+    o.orderdate,
+    o.shipdate,
+    o.shipmode,
+    o.ordersellingprice,
+    o.ordercustprice,
+    o.ordersellingprice - ordercustprice as orderprofit,
+    -- from raw customer
+    c.customername,
+    c.segment,
+    c.country,
+    -- from raw product
+    p.category,
+    p.productname,
+    p.subcategory
+from {{ ref("raw_orders") }} o
+left join {{ ref("raw_customer") }} c
+on o.customerid = c.customerid
+left join {{ ref("raw_product") }} p
+on o.productid = p.productid
