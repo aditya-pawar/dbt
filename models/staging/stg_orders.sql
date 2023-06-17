8d6291126515b522a7a63eb9e0d1d156d3646c2a
Aditya Pawar <codes.aditya@gmail.com>
2023-06-17 21:44:42 +0000

    developed report profit by product and customer

--- models/staging/stg_orders.sql
+++ models/staging/stg_orders.sql
@@ -8,10 +8,12 @@ select
     o.ordercustprice,
     o.ordersellingprice - ordercustprice as orderprofit,
     -- from raw customer
+    c.customerid,
     c.customername,
     c.segment,
     c.country,
     -- from raw product
+    p.productid,
     p.category,
     p.productname,
     p.subcategory
