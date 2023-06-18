990ce476456b1b781a81f09c46a871eb1b124581
Aditya Pawar <codes.aditya@gmail.com>
2023-06-18 18:16:54 +0000

    seed and use of packages

--- models/staging/stg_orders.sql
+++ models/staging/stg_orders.sql
@@ -1,5 +1,6 @@
 select
     -- from raw order
+    {{ dbt_utils.generate_surrogate_key(["o.orderid", "c.customerid", "p.productid"]) }} sk_orders,
     o.orderid,
     o.orderdate,
     o.shipdate,
@@ -17,12 +18,13 @@ select
     p.category,
     p.productname,
     p.subcategory,
-    {{markup('ordersellingprice','ordercustprice')}} markup
-    -- ,{{target.name}}
-from {{ ref("raw_orders") }} o
-left join {{ ref("raw_customer") }} c
-on o.customerid = c.customerid
-left join {{ ref("raw_product") }} p
-on o.productid = p.productid
-{{limit_data_in_dev("to_timestamp(orderdate,'yyyy/mm/dd')")}}
+    {{ markup("ordersellingprice", "ordercustprice") }} markup,
+    d.delivery_team
 
+-- ,{{target.name}}
+from {{ ref("raw_orders") }} o
+left join {{ ref("raw_customer") }} c on o.customerid = c.customerid
+left join {{ ref("raw_product") }} p on o.productid = p.productid
+left join {{ ref('delivery_team') }} d on d.shipmode = o.shipmode
+
+{{ limit_data_in_dev("to_timestamp(orderdate,'yyyy/mm/dd')") }}
